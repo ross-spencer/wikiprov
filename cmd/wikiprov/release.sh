6c0626c099f271a41f7cc2f7203dd3a18e1b2c7d
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2021-01-03 17:40:41 +0100

    Add release script

---
+++ cmd/wikiprov/release.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -eux
+
+MOON="wikiprov"
+DIR="release"
+mkdir "$DIR"
+export GOOS=windows
+export GOARCH=386
+go build
+mv "$MOON".exe "${DIR}/${MOON}"-win386.exe
+export GOOS=windows
+export GOARCH=amd64
+go build
+mv "$MOON".exe "${DIR}/${MOON}"-win64.exe
+export GOOS=linux
+export GOARCH=amd64
+go build
+mv "$MOON" "${DIR}/${MOON}"-linux64
+export GOOS=darwin
+export GOARCH=386
+go build
+mv "$MOON" "${DIR}/${MOON}"-darwin386
+export GOOS=darwin
+export GOARCH=amd64
+go build
+mv "$MOON" "${DIR}/${MOON}"-darwinAmd64
+export GOOS=
+export GOARCH=
